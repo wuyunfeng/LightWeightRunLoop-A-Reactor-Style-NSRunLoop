ab58e494f3f1dab1a4f48f2e2c18288b26dd5ad0
wuyunfeng <wyfsky888@126.com>
2016-09-23 22:49:01 +0800

    add: add some function to LWPort

--- LightWeightFoundation/LWPort.h
+++ LightWeightFoundation/LWPort.h
@@ -8,10 +8,86 @@
 
 #import <Foundation/Foundation.h>
 
+@class LWPortMessage;
+@class LWRunLoop;
+
+@protocol LWPortDelegate <NSObject>
+
+@optional
+
+- (void)handlePortMessage:( LWPortMessage * _Nullable )message;
+
+@end
+
+
 @interface LWPort : NSObject
+
+@property (nullable, assign, nonatomic) id<LWPortDelegate> delegate;
+
++ (LWPort * _Nullable)port;
+
+- (void)invalidate;
+
+@property (readonly, getter=isValid) BOOL valid;
+
+- (void)scheduleInRunLoop:(LWRunLoop * _Nonnull)runLoop forMode:(LWRunLoop * _Nonnull)mode;
+
+- (void)removeFromRunLoop:(LWRunLoop * _Nonnull)runLoop forMode:(LWRunLoop * _Nonnull)mode;
 
 @end
 
 @interface LWPortMessage : NSObject
 
+@property (nullable, readonly, copy) NSArray *components;
+@property (nullable, readonly, retain) NSPort *receivePort;
+@property (nullable, readonly, retain) NSPort *sendPort;
+
+@property uint32_t msgid; //The identifier for the receiver
+
+/**
+ *  Attempts to send the message before aDate, returning YES if successful or NO if the operation times out.
+ *
+ *  @param delay The delay second before which the message should be sent.
+ *
+ *  @return YES true if the operation is successful, otherwise NO false (for example, if the operation times out). 
+ *  @note Always return true.
+ *
+ */
+- (BOOL)sendBeforeDate:(NSInteger)delay;
+
+/**
+ *  Initializes a newly allocated LWPortMessage object to send given data on a given port and to receiver replies on another given port.
+ *
+ *  @param sendPort   The port on which the message is sent.
+ *  @param replyPort  The port on which replies to the message arrive.
+ *  @param components The data to send in the message
+ *
+ *  @return An LWPortMessage object initialized to send components on sendPort and to receiver replies on receivePort.
+ */
+- (_Nullable instancetype)initWithSendPort:(nullable NSPort *)sendPort receivePort:(nullable NSPort *)replyPort components:(nullable NSArray *)components;
+
 @end
+
+
+@interface LWSocketPort : LWPort
+
+- (nullable instancetype)initWithTCPPort:(unsigned short)port;
+
+// AF_INET AF_UNIX
+@property (readonly) int protocolFamily;
+
+//current support TCP socket, not support Unix Domain Socket
+@property (readonly) int socketType;
+
+@property (readonly) int protocol;
+
+@property (nullable, readonly, copy) NSData *address;
+
+//int act as NSSocketNativeHandle(typedef int )
+@property (readonly) int socket;
+
+
+@end
+
+
+
--- LightWeightFoundation/LWPort.m
+++ LightWeightFoundation/LWPort.m
@@ -7,12 +7,176 @@
 //
 
 #import "LWPort.h"
+#import <sys/socket.h>
+#import <netinet/in.h>
 
 @implementation LWPort
+{
+    BOOL _isValid;
+}
+
++ (LWPort * _Nullable)port
+{
+    LWPort *instance = [[LWPort alloc] init];
+    return instance;
+}
+
+- (void)invalidate
+{
+    _isValid = NO;
+}
+
+- (BOOL)isValid
+{
+    return _isValid;
+}
+
+- (void)scheduleInRunLoop:(LWRunLoop * _Nonnull)runLoop forMode:(LWRunLoop * _Nonnull)mode
+{
+    NSAssert(false, @"Must be implemented in subclass");
+}
+
+- (void)removeFromRunLoop:(LWRunLoop * _Nonnull)runLoop forMode:(LWRunLoop * _Nonnull)mode
+{
+    NSAssert(false, @"Must be implemented in subclass");
+}
 
 @end
 
 @implementation LWPortMessage
+{
+    NSArray *_components;
+    NSPort *_receivePort;
+    NSPort *_sendPort;
+}
+
+- (NSArray *)components
+{
+    return _components;
+}
+
+- (NSPort *)receivePort
+{
+    return _receivePort;
+}
+
+- (NSPort *)sendPort
+{
+    return _sendPort;
+}
+
+- (BOOL)sendBeforeDate:(NSInteger)delay
+{
+    return YES;
+}
+
+- (_Nullable instancetype)initWithSendPort:(nullable NSPort *)sendPort receivePort:(nullable NSPort *)replyPort components:(nullable NSArray *)components
+{
+    
+    return nil;
+}
+
+@end
+
+
+@implementation LWSocketPort
+{
+    int _potocolFamily;
+    int _socketType;
+    int _protocol;
+    
+    NSString *_ipStr;
+    unsigned short _port;
+    
+    int _sockFd;
+    
+    NSData *_address;
+    
+    //Leader-Follower Flag
+    int _flag;
+}
+
+- (nullable instancetype)initWithTCPPort:(unsigned short)port
+{
+    if (self = [super init]) {
+        _port = port;
+        if (![self initInternal]) {
+            return nil;
+        }
+    }
+    return self;
+}
+
+- (BOOL)initInternal
+{
+    if ((_sockFd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        return NO;
+    }
+    
+    struct sockaddr_in sockAddr;
+    memset(&sockAddr, 0, sizeof(sockAddr));
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    sockAddr.sin_port = htons(_port);
+    if (-1 == bind(_sockFd, (struct sockaddr *)&sockAddr, sizeof(sockAddr))) {
+        //socket is already bound to an address and the protocol does not support binding to a new address
+        if (EINVAL == errno) {
+            _flag = 1;//follower
+        } else {
+            return NO;
+        }
+    }
+    _flag = 0;//leader
+    
+    if (_flag == 0) {
+        if (listen(_sockFd, 5) == -1) {
+            return NO;
+        }
+    } else {
+        //we can ignore the `connect` delay for the local TCP connect
+        if (-1 == connect(_sockFd, (struct sockaddr *)&sockAddr, sizeof(sockAddr))) {
+            return NO;
+        }
+    }
+    
+    [self makeSocketNonBlocking:_sockFd];
+    return YES;
+}
+
+
+- (BOOL)makeSocketNonBlocking:(int)fd
+{
+    int flags;
+    if ((flags = fcntl(fd, F_GETFL, NULL)) < 0) {
+        return NO;
+    }
+    if (fcntl(fd, F_SETFL, flags | O_NONBLOCK) == -1) {
+        return NO;
+    }
+    return YES;
+}
+
+- (int)socket
+{
+    return _sockFd;
+}
+
+- (int)protocolFamily
+{
+    return _protocol;
+}
+
+- (int)protocol
+{
+    return _protocol;
+}
+
+- (int)socketType
+{
+    return _socketType;
+}
 
 
 @end
+
+
